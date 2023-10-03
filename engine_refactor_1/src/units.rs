e1fd3d01090ea7236d4304603c5ef3ade5b3f77d
George Johnson <johnsong@mssm.org>
2023-10-03 00:35:43 -0400

    Update units.rs
    Terrain modifiers

--- engine_refactor_1/src/units.rs
+++ engine_refactor_1/src/units.rs
@@ -1,29 +1,88 @@
-pub struct Units {
+pub struct Unit {
     hp: usize,
-    soft_attack: usize, // attacks/damage are multipliers against defense 
+    soft_attack: usize,
     hard_attack: usize,
     aa_damage: usize,
-    // armor: usize, ///resistance to hard attack
     defense: usize,
     movement: usize,
-    location: (usize, usize, usize),
-    
-} 
+    terrain_modifiers: [TerrainModifier; 3], // Three terrain modifiers
+}
 
-impl Units {
-    // fn take_damage() {
-    // }
+impl Unit {
+    fn new(
+        hp: usize,
+        soft_attack: usize,
+        hard_attack: usize,
+        aa_damage: usize,
+        defense: usize,
+        movement: usize,
+    ) -> Self {
+        // Initialize terrain modifiers for each terrain type
+        let terrain_modifiers = [
+            TerrainModifier {
+                terrain_type: Terrain::Plain,
+                movement: 1,
+                defense: 5,
+            },
+            TerrainModifier {
+                terrain_type: Terrain::Mountain,
+                movement: 10000,
+                defense: 10,
+            },
+            TerrainModifier {
+                terrain_type: Terrain::Coast,
+                movement: 10000,
+                defense: 2,
+            },
+            TerrainModifier {
+                terrain_type: Terrain::Forest,
+                movement: 2,
+                defense: 2,
+            },
+        ];
 
-
-    fn tank() -> Self {
         Units {
-            hp: 100,
-            soft_attack: 10,
-            hard_attack: 15,
-            aa_damage: 5,
-            defense: 10,
-            movement: 2,
-            location: (0,0,0),
+            hp,
+            soft_attack,
+            hard_attack,
+            aa_damage,
+            defense,
+            movement,
+            terrain_modifiers,
         }
     }
+}
+fn tank() -> Self {
+    let terrain_modifiers = [
+            TerrainModifier {
+                terrain_type: Terrain::Plain,
+                movement: 1,
+                defense: 5,
+            },
+            TerrainModifier {
+                terrain_type: Terrain::Mountain,
+                movement: 10000,
+                defense: 10,
+            },
+            TerrainModifier {
+                terrain_type: Terrain::Coast,
+                movement: 10000,
+                defense: 2,
+            },
+            TerrainModifier {
+                terrain_type: Terrain::Forest,
+                movement: 2,
+                defense: 2,
+            },
+        ];
+    Units {
+        hp: 100,
+        soft_attack: 10,
+        hard_attack: 15,
+        aa_damage: 5,
+        defense: 10,
+        movement: 2,
+        terrain_modifiers, 
+    }
+}
 }
