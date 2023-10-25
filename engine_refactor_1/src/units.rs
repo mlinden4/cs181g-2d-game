8d5b504678d5cd031a69d39ebcf4423c05bee077
tsawsum <johnsong@mssm.org>
2023-10-24 21:47:59 -0700

    bfs plus most of the pathing logic is build now. Based on the remaining movement of a unit

--- engine_refactor_1/src/units.rs
+++ engine_refactor_1/src/units.rs
@@ -3,8 +3,18 @@ use std::ops::Deref;
 use crate::tile::TerrainModifier;
 use crate::tile::Terrain;
 use crate::tile::Tile;
+use chickenwire::prelude::MultiCoord;
 use chickenwire::coordinate;
 use chickenwire::prelude::HexGrid;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use std::collections::HashMap;
+
+
+pub struct Itinerary {
+    coordinate: coordinate::Cube,
+    distance: i32
+}
 
 #[derive(PartialEq)]
 pub struct Unit {
@@ -14,7 +24,8 @@ pub struct Unit {
     hard_attack: usize,
     aa_damage: usize,
     defense: usize,
-    movement: usize,
+    pub movement: usize,
+    pub remaining_movement: usize,
     terrain_modifiers: [TerrainModifier; 4], // Three terrain modifiers
     pub location: coordinate::MultiCoord
 }
@@ -53,6 +64,7 @@ impl Unit {
                 defense: 2,
             },
         ];
+        let remaining_movement = movement;
 
         Unit {
             name,
@@ -62,6 +74,7 @@ impl Unit {
             aa_damage,
             defense,
             movement,
+            remaining_movement,
             terrain_modifiers,
             location
         }
@@ -89,6 +102,7 @@ impl Unit {
                     defense: 2,
                 },
             ];
+
         Unit {
             name: "Tank".into(),
             hp: 100,
@@ -97,6 +111,7 @@ impl Unit {
             aa_damage: 5,
             defense: 10,
             movement: 2,
+            remaining_movement: 2,
             terrain_modifiers, 
             location: location,
         }
@@ -125,17 +140,100 @@ impl Unit {
         //     let origin_cube =self.location.to_cube().unwrap();
         //     let dest_cube = destination.to_cube().unwrap();
         //     let dist = origin_cube.dist(dest_cube);
-
         //     dist <= self.movement as i32
         // } else {
         //     false
         // }
         // will eventually include dijkstra and terrain cost movement, will do a loop and bfs
-
-
         // also check to see if another unit is in tile, return false 
         // is distance to dest and compare with movement
     }
+
+    pub fn bfs(&self, grid: &HexGrid<Tile>, enemy_units : Vec<Unit>, allied_units : Vec<Unit>) -> Vec<(Itinerary)> {
+        let origin_cube = self.location.to_cube().unwrap();
+        
+        let mut visited = HashSet::new(); // To keep track of visited coordinates
+        let mut queue = VecDeque::new(); // Queue for BFS traversal
+        let mut distances = HashMap::new();
+    
+        queue.push_back(origin_cube);
+        visited.insert(origin_cube);
+        distances.insert(origin_cube, 0);
+    
+        let mut result = Vec::new();
+    
+        while !queue.is_empty() {
+            // Dequeue the current coordinate from the front of the queue
+            let current: chickenwire::prelude::Cube = queue.pop_front().unwrap();
+            let current_distance: i32 = distances[&current];
+    
+            // Process neighbors
+            for neighbor in current.neighbors() {
+                if !visited.contains(&neighbor) {
+                    // You can add conditions here to filter valid neighbors based on movement costs
+                    let mut dist: i32 = 1; 
+                    // Make if statements for each tile's movement cost
+                    let tile = grid.get(MultiCoord::from(neighbor)).unwrap();
+                    
+                    // TODO, ONLY CALL THIS LINE IF ITS NOT A HELICOPTER
+                    if tile.terrain == Terrain::Coast { 
+                        dist = 999;
+                    }
+                    if tile.terrain == Terrain::Plain { 
+                        dist = 1;
+                    }
+                    if tile.terrain == Terrain::Mountain { 
+                        dist = 999;
+                    }
+                    // TODO: SPECIAL ONE FOR TANKS SO FORESTS ARE 2 COST FOR THEM
+                    if tile.terrain == Terrain::Forest { 
+                        dist = 1;
+                    }
+                    // Make if statement if there is opponents unit
+                    for unit in &enemy_units { 
+                        if unit.location == MultiCoord::from(neighbor) {
+                            dist = 999;
+                        }
+                    }
+                    
+                    let neighbor_distance: i32 = current_distance + dist;
+
+                    if neighbor_distance <= self.remaining_movement.try_into().unwrap() {
+                        // Add neighbor to the queue and mark it as visited
+                        queue.push_back(neighbor);
+                        visited.insert(neighbor);
+    
+                        // Update the distance to the neighbor in the distances map
+                        distances.insert(neighbor, neighbor_distance);
+    
+                        // Create an Itinerary struct and add it to the result
+                        result.push(Itinerary {
+                            coordinate: neighbor,
+                            distance: neighbor_distance,
+                        });
+                    }
+    
+                  
+    
+                    // Just add the neighbor to the result with distance of dist
+                    let itin: Itinerary = Itinerary{coordinate : neighbor, distance : dist};
+                    result.push(itin);
+                }
+            }
+        }
+
+        // Update distances to 999 for allied units
+        for unit in &allied_units {
+            for itinerary in &mut result {
+                if itinerary.coordinate == unit.location.to_cube().unwrap() {
+                    itinerary.distance = 999;
+                }
+            }
+        }
+    
+        result
+    }
+    
 
     pub fn move_unit(&mut self, destination: coordinate::MultiCoord, grid: &mut HexGrid<Tile>) {
         if self.inrange(destination, grid) {
